103743821b000c68692712ce523a09f23a5cfdd9
Vittra <thomas.bjorksund@gmail.com>
2018-12-05 09:59:26 +0100

    updated branch

---
+++ orig.js
@@ -0,0 +1,60 @@
+window.onload = function(){
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var windowWidth = $(document).width();
+    var windowHeight = $(document).height();
+
+    var secondBoxScroll = $(".project2Wrapper").offset().top;
+    var secondBoxheight = $(".project2Wrapper").height();
+
+    var thirdBoxScroll = $(".project3Wrapper").offset().top;
+    var thirdBoxheight = $(".project3Wrapper").height();
+
+    var fourthBoxScroll = $(".project4Wrapper").offset().top;
+    var fourthBoxheight = $(".project4Wrapper").height();
+
+    var fifthBoxScroll = $(".project5Wrapper").offset().top;
+    var fifthBoxheight = $(".project5Wrapper").height();
+
+    $("li").click( function(j){
+      $("li").each( function(i){
+        if(j.html = "Case 0"+i){
+          console.log("click"+i);
+        }
+      })
+    });
+
+    $(window).keydown(function(keyPressed) {
+      if (keyPressed.keyCode == 78) {
+        $("#scrollBar").toggleClass("rainbow")
+      }
+    });
+    $(window).scroll(function(){
+      var toppen = $(window).scrollTop();
+      var loadBar = windowWidth/(windowHeight-$(window).height())*toppen
+
+      $("#scrollBar").css(
+        "width", loadBar
+      );
+      function isInView(distanceStart, distanceEnd) {
+          return toppen > distanceStart/5 && toppen < distanceStart+distanceEnd/50 && $('body').width() > 800;
+      }
+      if(isInView(secondBoxScroll, secondBoxheight)){
+      $(".project2Wrapper .imageBuff").css({
+        'transform' : 'scale(3) translateX(' + (((toppen-secondBoxScroll)*-windowWidth/1000)/2) + "px)"
+      })};
+      if(isInView(thirdBoxScroll, thirdBoxheight)){
+      $(".project3Wrapper .imageBuff").css({
+        'transform' : 'scale(3) translateX(' + (((toppen-thirdBoxScroll)*-windowWidth/1000)/2)*-1 + "px)"
+      })};
+      if(isInView(fourthBoxScroll, fourthBoxheight)){
+      $(".project4Wrapper .imageBuff").css({
+        'transform' : 'scale(3) translateX(' + (((toppen-fourthBoxScroll)*-windowWidth/1000)/2) + "px)"
+      })};
+      if(isInView(fifthBoxScroll, fifthBoxheight)){
+      $(".project5Wrapper .imageBuff").css({
+        'transform' : 'scale(3) translateX(' + (((toppen-fifthBoxScroll)*-windowWidth/1000)/2)*-1 + "px)"
+      })};
+    })
+});
+};
